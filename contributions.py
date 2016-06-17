12fee1781a9ff0ea6ed402c85c63064d454a5e2c
lordluen <lordluen@gmail.com>
2016-06-16 21:20:18 -0400

    made a visual

--- contributions.py
+++ contributions.py
@@ -11,6 +11,9 @@ import pandas as pd
 import xml.etree.ElementTree as ET
 from tqdm import tqdm
 from sqlCommands import append_to_database
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+import re
 
 from dateutil.parser import parse as DateParser
 import os
@@ -24,6 +27,7 @@ FIELD_SPECS={
     'received': DateParser,
     'contributiondate': DateParser,
     'contributiontype': str,
+    'registrantname': str,
     'honoree': str,
 }
 
@@ -77,8 +81,11 @@ def read_contribution_filings(datadir, dbname, engine, start_year=None):
                 abspath = os.path.join(datadir, filename)
                 print(abspath)
                 with open(abspath) as fd:
-                    filedata = parse_contribution_filing(fd) #get contributions from file as pd.df.
-                    append_to_database(dbname,'contrib',filedata,engine)
+                    try:
+                        filedata = parse_contribution_filing(fd) #get contributions from file as pd.df.
+                        append_to_database(dbname,'contrib',filedata,engine)
+                    except:
+                        pass
     return
 
 
@@ -88,6 +95,53 @@ def contribution_filings(dbname,engine,datadir=DATADIR, start_year=None):
     read_contribution_filings(datadir, dbname, engine, start_year)
     print('leaving contribution_filings')
     return
+
+def pull_contributions(legis,legis_all,engine):
+    #will return a datafram with all contributions to a given legislator
+    #legis is the legislator dataframe
+    #i is the row in question (legislator we care about)
+    fname = legis.fname.iloc[0]
+    lname = legis.lname.iloc[0]
+    station = legis.station.iloc[0]
+    name = legis.qsponsor.iloc[0]
+    #clean name for query
+    fname = re.sub("'","''",fname)
+    lname = re.sub("'","''",lname)
+    perfectQ = "SELECT * FROM contrib WHERE honoree LIKE '%%"+fname+"%%' AND honoree LIKE '%%"+lname+"%%' AND contributiontype LIKE 'FECA' ;" #definite
+    lessQ = "SELECT * FROM contrib WHERE honoree LIKE '%%"+lname+"%%';" #definite + maybe
+    perfectdf = pd.read_sql_query(perfectQ,engine)
+    lessdf = pd.read_sql_query(lessQ,engine)
+    maybedf = pd.concat([perfectdf,lessdf],axis=0).drop_duplicates(keep=False) #maybe
+
+    finaldf = perfectdf #will concatanate successful matches
+    #reduce total number of possible legislators to fuzzy
+    fuzz_legis = legis_all[legis_all.lname == lname]['qsponsor'].tolist()
+
+    #print(fuzz_legis)
+    matchbool = list()
+    matchstr = list()
+
+    #reduce list if it addresses the wrong station
+    if station == 'Rep':
+        opp_station = 'Sen'
+    if station == 'Sen':
+        opp_station = 'Rep'
+    print(station)
+    print(opp_station)
+    bools = [not i for i in maybedf.honoree.str.contains(opp_station)] #if it contains opposing station, then not correct.
+    maybedf2 = maybedf[bools]
+    #maybedf2
+    for c in range(maybedf2.index.size):
+        con = maybedf2.honoree.iloc[c]
+        match = process.extractOne(con,fuzz_legis)
+        if (match[0] ==  name) and match[1] > 55:
+            matchbool.append(1)
+        else:
+            matchbool.append(0)
+    matchbool = np.array(matchbool).astype(bool)
+    maybedf3 = maybedf2[matchbool]
+    return maybedf3
+
 
 if __name__ == "__main__":
     contribution_filings()
